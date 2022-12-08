c47cd1292d7f2cef9e5577199128bb519ccb14a0
AhlaamK-tap <a.kazi@tap.company>
2022-12-08 12:31:45 +0300

    condition modified

--- library/src/main/java/company/tap/gosellapi/internal/viewholders/RecentSectionViewHolder.java
+++ library/src/main/java/company/tap/gosellapi/internal/viewholders/RecentSectionViewHolder.java
@@ -47,9 +47,7 @@ public class RecentSectionViewHolder
         linearLayoutManager = new LinearLayoutManager(itemView.getContext(), LinearLayoutManager.HORIZONTAL, false);
         recentPaymentsRecyclerView.setLayoutManager(linearLayoutManager);
         for(int i=0;  i<data.size();i++){
-            if ((data != null && PaymentDataSource.getInstance().getCardType() != null) && PaymentDataSource.getInstance().getCardType().toString().equals("ALL")) {
-
-            }else if ((data != null && PaymentDataSource.getInstance().getCardType() != null) && !PaymentDataSource.getInstance().getCardType().toString().equals(data.get(i).getFunding())) {
+           if ((data != null && PaymentDataSource.getInstance().getCardType() != null) && !PaymentDataSource.getInstance().getCardType().toString().equals("ALL") && !PaymentDataSource.getInstance().getCardType().toString().equals(data.get(i).getFunding())) {
             data.remove(i);
             }
         }
