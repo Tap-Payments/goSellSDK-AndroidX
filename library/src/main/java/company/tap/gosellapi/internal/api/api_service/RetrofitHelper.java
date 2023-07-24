ca383dd069de21188f3abd6c4991c3beb8658651
aslm-android <130293799+aslm-android@users.noreply.github.com>
2023-07-24 14:58:35 +0300

    open_logs

--- library/src/main/java/company/tap/gosellapi/internal/api/api_service/RetrofitHelper.java
+++ library/src/main/java/company/tap/gosellapi/internal/api/api_service/RetrofitHelper.java
@@ -50,6 +50,7 @@ public final class RetrofitHelper {
             }
 
             OkHttpClient okHttpClient = getOkHttpClient();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(API_Constants.BASE_URL)
                     .addConverterFactory(buildGsonConverter())
@@ -91,7 +92,7 @@ public final class RetrofitHelper {
                     .addHeader(API_Constants.CONTENT_TYPE_KEY, API_Constants.CONTENT_TYPE_VALUE).build();
             return chain.proceed(request);
         });
-        httpClientBuilder.addInterceptor(new HttpLoggingInterceptor().setLevel(!BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.NONE : HttpLoggingInterceptor.Level.NONE));
+        httpClientBuilder.addInterceptor(new HttpLoggingInterceptor().setLevel(!BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.BODY));
 
         return httpClientBuilder.build();
     }
