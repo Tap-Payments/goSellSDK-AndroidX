2168bb5f3770c155cb2587ea22caaff92ba8a432
AhlaamK-tap <57221514+AhlaamK-tap@users.noreply.github.com>
2025-08-27 09:55:10 +0300

    updating code for locale in baseactivty

--- goSellSDKSample/src/main/java/company/tap/sample/activity/MainActivity.java
+++ goSellSDKSample/src/main/java/company/tap/sample/activity/MainActivity.java
@@ -155,7 +155,7 @@ public class MainActivity extends AppCompatActivity implements SessionDelegate {
      */
     private void configureApp() {
         GoSellSDK.init(this, "sk_test_kovrMB0mupFJXfNZWx6Etg5y", "company.tap.goSellSDKExample");  // to be replaced by merchant
-       GoSellSDK.setLocale(this,"ar");//  language to be set by merchant
+      // GoSellSDK.setLocale(this,"ar");//  language to be set by merchant
 
     }
 
