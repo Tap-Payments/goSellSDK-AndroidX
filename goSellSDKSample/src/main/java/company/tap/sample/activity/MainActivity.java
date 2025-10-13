e5d067ade41096caf0b9e71bc24b14212d191226
AhlaamK-tap <57221514+AhlaamK-tap@users.noreply.github.com>
2025-10-13 10:05:43 +0300

    update security

--- goSellSDKSample/src/main/java/company/tap/sample/activity/MainActivity.java
+++ goSellSDKSample/src/main/java/company/tap/sample/activity/MainActivity.java
@@ -154,8 +154,8 @@ public class MainActivity extends AppCompatActivity implements SessionDelegate {
      * Configure SDK with your Secret API key and App Bundle name registered with tap company.
      */
     private void configureApp() {
-      //  GoSellSDK.init(this, "sk_test_kovrMB0mupFJXfNZWx6Etg5y", "company.tap.goSellSDKExample");  // to be replaced by merchant
-        GoSellSDK.init(this, "sk_live_8Bo2kTt46lSEMx3dUpncKui0", "com.si.fitplayapp");  // to be replaced by merchant
+        GoSellSDK.init(this, "sk_test_XXXX", "company.tap.goSellSDKExample");  // to be replaced by merchant
+
       // GoSellSDK.setLocale(this,"en");//  language to be set by merchant
 
     }
